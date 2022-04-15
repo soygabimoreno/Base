2157bcb15f04db2cc6829fb0f57d0225311777d1
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-15 10:43:36 +0200

    Bike app added

---
+++ bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
@@ -0,0 +1,12 @@
+package soy.gabimoreno.bike.ui.views
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Box {
+        Text(text = "Bike")
+    }
+}
