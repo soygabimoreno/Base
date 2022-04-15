7ebe6841ccf461e07587f8dc88c5ba9c58a66c40
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-15 10:43:36 +0200

    Bike app added

---
+++ bike/src/main/java/soy/gabimoreno/bike/presentation/MainActivity.kt
@@ -0,0 +1,27 @@
+package soy.gabimoreno.bike.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import soy.gabimoreno.bike.ui.theme.BaseTheme
+import soy.gabimoreno.bike.ui.views.MainScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BaseTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
