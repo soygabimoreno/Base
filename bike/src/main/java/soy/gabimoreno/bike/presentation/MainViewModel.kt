4393a67d5921f74bfcc82d4cae8f449d13f2fc46
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-19 08:19:05 +0200

    Hilt configured

--- bike/src/main/java/soy/gabimoreno/bike/presentation/MainViewModel.kt
+++ bike/src/main/java/soy/gabimoreno/bike/presentation/MainViewModel.kt
@@ -1,18 +1,32 @@
 package soy.gabimoreno.bike.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import soy.gabimoreno.bike.di.IO
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    @IO private val dispatcher: CoroutineDispatcher,
+) : ViewModel() {
 
-    init {
-        checkBluetoothPermission()
+    private val _viewEvents = MutableStateFlow<ViewEvent>(ViewEvent.InitialState)
+    val viewEvents: StateFlow<ViewEvent> = _viewEvents.asStateFlow()
+
+    fun onNullBluetoothAdapter() {
+        viewModelScope.launch(dispatcher) {
+            _viewEvents.value = ViewEvent.ShowDeviceDoesNotSupportBluetooth
+        }
     }
 
-    fun checkBluetoothPermission() {
-
-
+    sealed class ViewEvent {
+        object InitialState : ViewEvent()
+        object ShowDeviceDoesNotSupportBluetooth : ViewEvent()
     }
 }
