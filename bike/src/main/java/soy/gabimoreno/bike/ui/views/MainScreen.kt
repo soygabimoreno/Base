c3374a4b0bc68e3c1a42625a8f572cb6ab941a4f
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-19 08:19:05 +0200

    Hilt configured

--- bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
+++ bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
@@ -7,25 +7,38 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import soy.gabimoreno.bike.R
 import soy.gabimoreno.bike.presentation.MainViewModel
+import soy.gabimoreno.framework.toast
 
 @Composable
-fun MainScreen() {
+fun MainScreen(mainViewModel: MainViewModel = hiltViewModel()) {
     val context = LocalContext.current
+    val viewEvents by mainViewModel.viewEvents.collectAsState()
+    when (viewEvents) {
+        MainViewModel.ViewEvent.InitialState -> {
+            context.toast(stringResource(id = R.string.initialized))
+        }
+        MainViewModel.ViewEvent.ShowDeviceDoesNotSupportBluetooth -> {
+            context.toast(stringResource(id = R.string.device_does_not_support_bluetooth))
+        }
+    }
+
     val bluetoothManager: BluetoothManager = context.getSystemService(BluetoothManager::class.java)
     val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
     if (bluetoothAdapter == null) {
-        // Device doesn't support Bluetooth
+        mainViewModel.onNullBluetoothAdapter()
     }
 
-    val mainViewModel: MainViewModel = hiltViewModel()
     Box(
         Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -45,13 +58,4 @@ fun MainScreen() {
             modifier = Modifier.padding(16.dp)
         )
     }
-}
-
-@Preview(
-    widthDp = 540,
-    heightDp = 960
-)
-@Composable
-fun Preview() {
-    MainScreen()
 }
---
+++ modules/framework/src/main/java/soy/gabimoreno/framework/ContextExtensions.kt
@@ -0,0 +1,27 @@
+package soy.gabimoreno.framework
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+fun Context.toast(
+    @StringRes resId: Int,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    Toast.makeText(
+        this,
+        resId,
+        duration
+    ).show()
+}
+
+fun Context.toast(
+    message: CharSequence,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    Toast.makeText(
+        this,
+        message,
+        duration
+    ).show()
+}
