1965f29cf8a2219eac88bb51c9ac5416ca2e1bbd
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-22 07:03:28 +0200

    Launcher icon and start to listen the events from main activity

--- bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
+++ bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
@@ -1,44 +1,19 @@
 package soy.gabimoreno.bike.ui.views
 
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import soy.gabimoreno.bike.R
 import soy.gabimoreno.bike.presentation.MainViewModel
-import soy.gabimoreno.framework.toast
 
 @Composable
 fun MainScreen(mainViewModel: MainViewModel = hiltViewModel()) {
-    val context = LocalContext.current
-    val viewEvents by mainViewModel.viewEvents.collectAsState()
-    when (viewEvents) {
-        MainViewModel.ViewEvent.InitialState -> {
-            context.toast(stringResource(id = R.string.initialized))
-        }
-        MainViewModel.ViewEvent.ShowDeviceDoesNotSupportBluetooth -> {
-            context.toast(stringResource(id = R.string.device_does_not_support_bluetooth))
-        }
-    }
-
-    val bluetoothManager: BluetoothManager = context.getSystemService(BluetoothManager::class.java)
-    val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
-    if (bluetoothAdapter == null) {
-        mainViewModel.onNullBluetoothAdapter()
-    }
-
     Box(
         Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
