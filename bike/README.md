ee158dccc0e23fc6ed32ce7b327f66fe97fc8099
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-26 21:17:09 +0200

    The dataString is refreshed

---
+++ bike/README.md
@@ -0,0 +1,1 @@
+This project uses [FastBle](https://github.com/Jasonchenlijian/FastBle) library. Thanks for creating such an amazing tool, [Jasonchenlijian](https://github.com/Jasonchenlijian).
--- bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
+++ bike/src/main/java/soy/gabimoreno/bike/ui/views/MainScreen.kt
@@ -1,6 +1,5 @@
 package soy.gabimoreno.bike.ui.views
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
@@ -15,28 +14,13 @@ import soy.gabimoreno.bike.presentation.MainViewModel
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
     val viewState by viewModel.viewState.collectAsState()
-    val dataString = (viewState as? MainViewModel.ViewState.Content)?.dataString ?: ""
-    Log.d(
-        "FOO",
-        "MainScreen, dataString: $dataString"
-    )
     Box(
         Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(
             fontSize = 128.sp,
-            text = "21"
+            text = (viewState as? MainViewModel.ViewState.Content)?.dataString ?: ""
         )
-    }
-
-    Box(
-        Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter
-    ) {
-//        Text(
-//            text = "Bike",
-//            modifier = Modifier.padding(16.dp)
-//        )
     }
 }
