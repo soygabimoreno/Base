39fddf8ba846418de68837dac90f58c5cb24bf1e
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-05-03 08:23:35 +0200

    Gradle files migrated to kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,12 +10,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven(url = "https://jitpack.io")
     }
 }
 rootProject.name = "Base"
-include ':gabimoreno'
-include ':bike'
+include(":gabimoreno")
+include(":bike")
 
-include ':modules:framework'
-include ':modules:player'
+include(":modules:framework")
+include(":modules:player")
