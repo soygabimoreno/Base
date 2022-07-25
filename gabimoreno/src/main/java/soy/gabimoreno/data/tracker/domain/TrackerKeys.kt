68f6d5a506b37c0316f75dbb95e21dcada3130be
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-07-25 06:39:15 +0200

    Ensure encode url

--- gabimoreno/src/main/java/soy/gabimoreno/data/tracker/domain/TrackerKeys.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/data/tracker/domain/TrackerKeys.kt
@@ -1,6 +1,8 @@
 package soy.gabimoreno.data.tracker.domain
 
-internal const val EPISODE_ID = "EPISODE_ID"
-internal const val EPISODE_TITLE = "EPISODE_TITLE"
-internal const val EPISODE_PLAYBACK_POSITION = "EPISODE_PLAYBACK_POSITION"
-internal const val WEB_VIEW_URL = "WEB_VIEW_URL"
+internal const val TRACKER_KEY_EPISODE_ID = "EPISODE_ID"
+internal const val TRACKER_KEY_EPISODE_TITLE = "EPISODE_TITLE"
+internal const val TRACKER_KEY_EPISODE_PLAYBACK_POSITION = "EPISODE_PLAYBACK_POSITION"
+internal const val TRACKER_KEY_WEB_VIEW_URL = "WEB_VIEW_URL"
+internal const val TRACKER_KEY_URL = "URL"
+internal const val TRACKER_KEY_EXCEPTION = "EXCEPTION"
---
+++ gabimoreno/src/main/java/soy/gabimoreno/domain/usecase/EncodeUrlUseCase.kt
@@ -0,0 +1,34 @@
+package soy.gabimoreno.domain.usecase
+
+import androidx.annotation.VisibleForTesting
+import soy.gabimoreno.data.tracker.Tracker
+import soy.gabimoreno.data.tracker.domain.TRACKER_KEY_EXCEPTION
+import soy.gabimoreno.data.tracker.domain.TRACKER_KEY_URL
+import soy.gabimoreno.data.tracker.main.ErrorTrackerEvent
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import javax.inject.Inject
+
+class EncodeUrlUseCase @Inject constructor(
+    private val tracker: Tracker,
+) {
+
+    operator fun invoke(url: String): String {
+        return try {
+            URLEncoder.encode(url, StandardCharsets.UTF_8.toString())
+        } catch (e: Exception) {
+            tracker.trackEvent(
+                ErrorTrackerEvent.EncodeUrlException(
+                    mapOf(
+                        TRACKER_KEY_URL to url,
+                        TRACKER_KEY_EXCEPTION to e.toString()
+                    )
+                )
+            )
+            GABI_MORENO_WEB_BASE_URL_ENCODED
+        }
+    }
+}
+
+@VisibleForTesting
+internal const val GABI_MORENO_WEB_BASE_URL_ENCODED = "https%3A%2F%2Fgabimoreno.soy"
