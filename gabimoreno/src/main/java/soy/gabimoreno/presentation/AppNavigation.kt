25b7d1d76f9b21ed25037fededa1ec67bf2976fc
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-07-27 08:23:47 +0200

    BottomBar is visible

--- gabimoreno/src/main/java/soy/gabimoreno/presentation/AppNavigation.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/presentation/AppNavigation.kt
@@ -1,4 +1,4 @@
-package soy.gabimoreno
+package soy.gabimoreno.presentation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
@@ -10,7 +10,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import soy.gabimoreno.presentation.navigation.Feature
 import soy.gabimoreno.presentation.navigation.NavArg
-import soy.gabimoreno.presentation.navigation.NavItem
+import soy.gabimoreno.presentation.navigation.NavCommand
 import soy.gabimoreno.presentation.screen.detail.DetailScreen
 import soy.gabimoreno.presentation.screen.home.HomeScreen
 import soy.gabimoreno.presentation.screen.webview.WebViewScreen
@@ -30,10 +30,10 @@ private fun NavGraphBuilder.podcastNav(
     navController: NavController
 ) {
     navigation(
-        startDestination = NavItem.ContentType(Feature.PODCAST).route,
+        startDestination = NavCommand.ContentType(Feature.PODCAST).route,
         route = Feature.PODCAST.route
     ) {
-        composable(navItem = NavItem.ContentType(Feature.PODCAST)) {
+        composable(navCommand = NavCommand.ContentType(Feature.PODCAST)) {
             HomeScreen(
                 onItemClicked = { episodeId ->
                     navController.navigateToDetail(episodeId)
@@ -49,7 +49,7 @@ private fun NavGraphBuilder.podcastNav(
 
         // TODO: Check how to manage deep links
         // deepLinks = listOf(navDeepLink { uriPattern = "https://gabimoreno.soy/{id}" })
-        composable(navItem = NavItem.ContentDetail(Feature.PODCAST)) {
+        composable(navCommand = NavCommand.ContentDetail(Feature.PODCAST)) {
             DetailScreen(
                 podcastId = it.findArg(NavArg.EpisodeId),
                 onBackClicked = {
@@ -58,7 +58,7 @@ private fun NavGraphBuilder.podcastNav(
             )
         }
 
-        composable(navItem = NavItem.ContentWebView(Feature.PODCAST)) {
+        composable(navCommand = NavCommand.ContentWebView(Feature.PODCAST)) {
             WebViewScreen(
                 url = it.findArg(NavArg.EncodedUrl),
                 onBackClicked = {
@@ -70,11 +70,11 @@ private fun NavGraphBuilder.podcastNav(
 }
 
 private fun NavController.navigateToDetail(episodeId: String) {
-    navigate(route = NavItem.ContentDetail(Feature.PODCAST).createRoute(episodeId))
+    navigate(route = NavCommand.ContentDetail(Feature.PODCAST).createRoute(episodeId))
 }
 
 private fun NavController.navigateToWebView(encodedUrl: String) {
-    navigate(route = NavItem.ContentWebView(Feature.PODCAST).createRoute(encodedUrl))
+    navigate(route = NavCommand.ContentWebView(Feature.PODCAST).createRoute(encodedUrl))
 }
 
 private fun NavController.goBack() {
@@ -82,12 +82,12 @@ private fun NavController.goBack() {
 }
 
 private fun NavGraphBuilder.composable(
-    navItem: NavItem,
+    navCommand: NavCommand,
     content: @Composable (NavBackStackEntry) -> Unit
 ) {
     composable(
-        route = navItem.route,
-        arguments = navItem.args
+        route = navCommand.route,
+        arguments = navCommand.args
     ) {
         content(it)
     }
--- gabimoreno/src/main/java/soy/gabimoreno/presentation/navigation/NavItem.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/presentation/navigation/NavItem.kt
@@ -1,16 +1,35 @@
 package soy.gabimoreno.presentation.navigation
 
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.compose.navArgument
+import soy.gabimoreno.R
 
-sealed class NavItem(
+enum class NavItem(
+    val navCommand: NavCommand,
+    val icon: ImageVector,
+    @StringRes val titleResId: Int
+) {
+    PODCAST(
+        NavCommand.ContentType(Feature.PODCAST),
+        Icons.Default.PlayArrow,
+        R.string.nav_item_podcast
+    ),
+    PREMIUM(NavCommand.ContentType(Feature.PREMIUM), Icons.Default.Star, R.string.nav_item_premium)
+}
+
+sealed class NavCommand(
     internal val feature: Feature,
     internal val subRoute: String = "home",
     private val navArgs: List<NavArg> = emptyList()
 ) {
-    class ContentType(feature: Feature) : NavItem(feature)
+    class ContentType(feature: Feature) : NavCommand(feature)
 
-    class ContentDetail(feature: Feature) : NavItem(
+    class ContentDetail(feature: Feature) : NavCommand(
         feature,
         "detail",
         listOf(NavArg.EpisodeId)
@@ -18,7 +37,7 @@ sealed class NavItem(
         fun createRoute(episodeId: String) = "${feature.route}/$subRoute/$episodeId"
     }
 
-    class ContentWebView(feature: Feature) : NavItem(
+    class ContentWebView(feature: Feature) : NavCommand(
         feature,
         "webView",
         listOf(NavArg.EncodedUrl)
