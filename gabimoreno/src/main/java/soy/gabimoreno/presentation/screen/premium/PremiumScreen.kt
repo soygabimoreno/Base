0e16503a5aae9a11474782c7079f4fead6c92a8c
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-08-23 08:09:55 +0200

    Added logic for the PREMIUM_SUBSCRIPTION_LAUNCH Feature Flag

--- gabimoreno/src/main/java/soy/gabimoreno/presentation/screen/premium/PremiumScreen.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/presentation/screen/premium/PremiumScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.flow.collect
 import soy.gabimoreno.R
 import soy.gabimoreno.presentation.screen.ViewModelProvider
 import soy.gabimoreno.presentation.theme.Spacing
@@ -14,9 +17,15 @@ import soy.gabimoreno.presentation.theme.Spacing
 @Composable
 fun PremiumScreen() {
     val premiumViewModel = ViewModelProvider.premiumViewModel
+    val showLogin = remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         premiumViewModel.onViewScreen()
+        premiumViewModel.viewEventFlow.collect { viewEvent ->
+            when (viewEvent) {
+                is PremiumViewModel.ViewEvent.ShowLogin -> showLogin.value = true
+            }
+        }
     }
 
     Column(
@@ -30,5 +39,9 @@ fun PremiumScreen() {
         )
         Spacer(modifier = Modifier.height(Spacing.s16))
         Text(text = stringResource(id = R.string.premium_description))
+        if (showLogin.value) {
+            Spacer(modifier = Modifier.height(Spacing.s16))
+            Text(text = stringResource(id = R.string.premium_login))
+        }
     }
 }
--- gabimoreno/src/test/java/soy/gabimoreno/presentation/screen/premium/PremiumViewModelTest.kt
+++ gabimoreno/src/test/java/soy/gabimoreno/presentation/screen/premium/PremiumViewModelTest.kt
@@ -1,30 +1,74 @@
 package soy.gabimoreno.presentation.screen.premium
 
+import io.mockk.every
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import soy.gabimoreno.core.testing.relaxedMockk
 import soy.gabimoreno.core.testing.verifyOnce
 import soy.gabimoreno.data.tracker.Tracker
 import soy.gabimoreno.data.tracker.main.PremiumTrackerEvent
+import soy.gabimoreno.remoteconfig.RemoteConfigName
+import soy.gabimoreno.remoteconfig.RemoteConfigProvider
 
+@ExperimentalCoroutinesApi
 class PremiumViewModelTest {
 
     private val tracker: Tracker = relaxedMockk()
+    private val remoteConfigProvider: RemoteConfigProvider = relaxedMockk()
     private lateinit var viewModel: PremiumViewModel
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 
     @Before
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
         viewModel = PremiumViewModel(
-            tracker
+            tracker,
+            remoteConfigProvider,
+            testDispatcher
         )
     }
 
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+    }
+
     @Test
-    fun `WHEN onViewScreen THEN track event`() {
+    fun `GIVEN PREMIUM_SUBSCRIPTION_LAUNCH enabled WHEN onViewScreen THEN track event and show login`() {
+        every { remoteConfigProvider.isFeatureEnabled(RemoteConfigName.PREMIUM_SUBSCRIPTION_LAUNCH) } returns true
+
         viewModel.onViewScreen()
 
         verifyOnce {
             tracker.trackEvent(PremiumTrackerEvent.ViewScreen)
+            // TODO: Test this navigation
+//            runTest(testDispatcher) {
+//                val viewEvent = viewModel.viewEventFlow.first()
+//                viewEvent shouldBe PremiumViewModel.ViewEvent.ShowLogin
+//            }
+        }
+    }
+
+    @Test
+    fun `GIVEN PREMIUM_SUBSCRIPTION_LAUNCH disabled WHEN onViewScreen THEN track event and do not show login`() {
+        every { remoteConfigProvider.isFeatureEnabled(RemoteConfigName.PREMIUM_SUBSCRIPTION_LAUNCH) } returns true
+
+        viewModel.onViewScreen()
+
+        verifyOnce {
+            tracker.trackEvent(PremiumTrackerEvent.ViewScreen)
+            // TODO: Test this navigation
+//            runTest(testDispatcher) {
+//                val viewEvent = viewModel.viewEventFlow.first()
+//                viewEvent shouldNotBe PremiumViewModel.ViewEvent.ShowLogin
+//            }
         }
     }
 }
