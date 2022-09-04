66ed6e14b793300ebe1d2facfcdfdcb9b4ef9187
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-09-04 16:09:08 +0200

    Bump version

--- gabimoreno/build.gradle.kts
+++ gabimoreno/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "soy.gabimoreno"
         minSdk = 23
         targetSdk = 32
-        versionCode = 25
-        versionName = "0.25.0"
+        versionCode = 26
+        versionName = "0.26.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
