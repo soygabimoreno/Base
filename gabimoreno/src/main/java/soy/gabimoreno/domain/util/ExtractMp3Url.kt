8b8fa49af45c3523c5b3edb321bb9318bcf5ebb1
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-09-19 07:31:31 +0200

    extractMp3Url improved

--- gabimoreno/src/main/java/soy/gabimoreno/domain/util/ExtractMp3Url.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/domain/util/ExtractMp3Url.kt
@@ -1,11 +1,17 @@
 package soy.gabimoreno.domain.util
 
+import androidx.annotation.VisibleForTesting
+
 fun String.extractMp3Url(): String? {
-    val startIndex = indexOf("https://")
-    val suffix = ".mp3"
+    val startIndex = indexOf(PREFIX)
+    val suffix = SUFFIX
     val suffixLength = suffix.length
     val endIndex = indexOf(suffix) + suffixLength
 
     if (startIndex == -1 || endIndex == suffixLength - 1) return null
     return substring(startIndex, endIndex)
 }
+
+@VisibleForTesting
+internal const val PREFIX = "https://gabimoreno.soy/wp-content/uploads/"
+internal const val SUFFIX = ".mp3"
--- gabimoreno/src/test/java/soy/gabimoreno/domain/util/ExtractMp3UrlKtTest.kt
+++ gabimoreno/src/test/java/soy/gabimoreno/domain/util/ExtractMp3UrlKtTest.kt
@@ -37,7 +37,27 @@ class ExtractMp3UrlKtTest {
 
         result shouldBeEqualTo null
     }
+
+    @Test
+    fun `GIVEN content with a link to subscribe before restricted audio block WHEN call THEN get the expected mp3 url`() {
+        val rendered =
+            "<div class=\"typography\">\n" +
+                "<p>Ya está disponible en el canal estable la nueva versión de Android Studio, la 2021.3.1 o más fácil:" +
+                " <strong>Dolphin</strong>.</p>\n\n\n\n" +
+                "<p>Si te <a href=\"https://gabimoreno.soy/suscribirme\">suscribes</a>, " +
+                "podrás escuchar el <strong>audio premium</strong> en el que te cuento esta noticia de viva voz. " +
+                "Así podrás estar al día del ecosistema Android mientras haces otras cosas.</p>\n\n\n\n" +
+                "<p>Además es muy entretenido. 😉</p>\n\n\n\n" +
+                "<figure class=\"wp-block-audio\"><audio controls src=\"$MP3_URL\"></audio></figure>\n\n\n\n\n" +
+                "<h2>Novedades en Jetpack Compose</h2>\n\n\n\n<h3>Compose Animation Coordination</h3>\n\n\n\n" +
+                "<p>Si tienes las animaciones descritas en un composable preview, ahora desde el Animation Preview" +
+                " se puede tanto inspeccionar como coordinar a la vez.</p>\n\n\n\n"
+
+        val result = rendered.extractMp3Url()
+
+        result shouldBeEqualTo MP3_URL
+    }
 }
 
 private const val MP3_URL =
-    "https://gabimoreno.soy/wp-content/uploads/GABI-MORENO-Premium-sample.mp3"
+    "${PREFIX}GABI-MORENO-Premium-sample.mp3"
