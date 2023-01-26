e109cdbc5ba2a536b2d5615b36fd5da01b8b5435
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2023-01-26 08:46:31 +0100

    Create unit tests for RemotePodcastRepository

--- gabimoreno/src/main/java/soy/gabimoreno/data/remote/datasource/podcast/PodcastDatasource.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/data/remote/datasource/podcast/PodcastDatasource.kt
@@ -1,8 +1,9 @@
 package soy.gabimoreno.data.remote.datasource.podcast
 
 import arrow.core.Either
+import soy.gabimoreno.di.data.PodcastUrl
 import soy.gabimoreno.domain.model.podcast.EpisodesWrapper
 
 interface PodcastDatasource {
-    suspend fun getEpisodes(url: String): Either<Throwable, EpisodesWrapper>
+    suspend fun getEpisodes(podcastUrl: PodcastUrl): Either<Throwable, EpisodesWrapper>
 }
