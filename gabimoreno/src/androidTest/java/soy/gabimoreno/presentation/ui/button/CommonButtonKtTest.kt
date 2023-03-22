af3d5dc4effa8e516af5b2a83a39235d91219d1d
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2023-03-22 19:10:18 +0100

    Mob Programming - Some Jetpack Compose Tests

--- gabimoreno/src/androidTest/java/soy/gabimoreno/presentation/ui/button/CommonButtonKtTest.kt
+++ gabimoreno/src/androidTest/java/soy/gabimoreno/presentation/ui/button/CommonButtonKtTest.kt
@@ -1,11 +1,15 @@
 package soy.gabimoreno.presentation.ui.button
 
+import androidx.compose.foundation.background
 import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
+import org.amshove.kluent.shouldBe
 import org.junit.Rule
 import org.junit.Test
+import soy.gabimoreno.presentation.theme.Orange
 import soy.gabimoreno.presentation.theme.Spacing
 import soy.gabimoreno.presentation.ui.PreviewContent
 
@@ -15,21 +19,85 @@ class CommonButtonKtTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun commonButtonTest() {
+    fun checkBoxIsDisplayedInCommonButtonTest() {
         val text = "Play"
         composeTestRule.setContent {
             PreviewContent {
                 CommonButton(
-                    text,
+                    text = text,
                     height = Spacing.s48,
-                    MaterialTheme.colors.primary
+                    background = MaterialTheme.colors.primary
                 ) {}
             }
         }
 
-        composeTestRule.onNodeWithTag(COMMON_BUTTON_BOX_ID)
+        composeTestRule
+            .onNodeWithTag(COMMON_BUTTON_BOX_ID)
             .assertIsDisplayed()
-//            .onChild()
-//            .assert(hasText(text))
+    }
+
+    @Test
+    fun checkTextIsDisplayedInCommonButtonTest() {
+        val text = "Play"
+        val height = Spacing.s48
+        composeTestRule.setContent {
+            PreviewContent {
+                CommonButton(
+                    text = text,
+                    height = height,
+                    background = MaterialTheme.colors.primary
+                ) {}
+            }
+        }
+
+        composeTestRule.onNode(hasTestTag(COMMON_BUTTON_BOX_ID), true)
+            .onChild()
+            .assert(hasText(text))
+    }
+
+    @Test
+    fun checkHeightInCommonButtonIsTheExpectedTest() {
+        val text = "Play"
+        val height = Spacing.s48
+        composeTestRule.setContent {
+            PreviewContent {
+                CommonButton(
+                    text = text,
+                    height = height,
+                    background = MaterialTheme.colors.primary
+                ) {}
+            }
+        }
+
+        composeTestRule.onNode(hasTestTag(COMMON_BUTTON_BOX_ID), true)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun checkBackgroundInCommonButtonIsTheExpectedTest() {
+        val text = "Play"
+        val height = Spacing.s48
+        val background = Orange
+        composeTestRule.setContent {
+            PreviewContent {
+                CommonButton(
+                    text = text,
+                    height = height,
+                    background = background
+                ) {}
+            }
+        }
+
+        val result = composeTestRule
+            .onNode(hasTestTag(COMMON_BUTTON_BOX_ID), true)
+            .hasBackground(background)
+
+        result shouldBe true
+    }
+
+    private fun SemanticsNodeInteraction.hasBackground(color: Color): Boolean {
+        return fetchSemanticsNode().layoutInfo.getModifierInfo().filter { modifierInfo ->
+            modifierInfo.modifier == Modifier.background(color)
+        }.size == 1
     }
 }
