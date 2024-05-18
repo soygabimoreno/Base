37b9d564819430ef7cc2ea3e228e819cedd8dfc1
Jimmy Saenz <jsaenzr@outlook.com>
2024-05-18 17:48:52 -0600

    Create custom component to make text selectable using the system gestures

---
+++ gabimoreno/src/androidTest/java/soy/gabimoreno/presentation/ui/SelectableEmphasisTextTest.kt
@@ -0,0 +1,29 @@
+package soy.gabimoreno.presentation.ui
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+internal class SelectableEmphasisTextTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun checkSelectableEmphasisTextIsDisplayed() {
+        val expectedText = "Hello, World!"
+
+        composeTestRule.setContent {
+            SelectableEmphasisText(
+                text = "Hello, World!"
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText(expectedText)
+            .assertExists()
+            .assertIsDisplayed()
+    }
+}
