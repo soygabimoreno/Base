b3895fbff2b7bffb87d95d816b2f6db5949b9d34
Raúl Luján Colilla <mcdrokz@gmail.com>
2025-04-18 08:45:14 +0200

    Refactor: Enable Edge to Edge

--- gabimoreno/src/main/java/soy/gabimoreno/presentation/screen/premium/PremiumScreen.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/presentation/screen/premium/PremiumScreen.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -150,6 +153,7 @@ fun PremiumScreen(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
         if (showAccess && !showPremium) {
             Column(modifier = Modifier.padding(Spacing.s16)) {
