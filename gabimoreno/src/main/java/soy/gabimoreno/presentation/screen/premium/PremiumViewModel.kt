4d2204156add3fa1d386f3a8a823fde8f882eead
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2025-04-20 16:22:15 +0200

    Bump version

--- gabimoreno/src/main/java/soy/gabimoreno/presentation/screen/premium/PremiumViewModel.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/presentation/screen/premium/PremiumViewModel.kt
@@ -175,8 +175,6 @@ class PremiumViewModel @Inject constructor(
         }
     }
 
-    // Commit in the main branch (just for learning purposes)
-
     private fun showTokenExpiredError() {
         state = state.copy(
             shouldShowAccess = true,
