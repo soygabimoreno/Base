1f7b5ad289855f78d52b7f2d5e9c0bf1e972f19b
Raúl Luján <81721889+kmorfo@users.noreply.github.com>
2025-05-11 21:29:57 +0200

    refactor: Improve composable key usage and bottom navigation padding

--- gabimoreno/src/main/java/soy/gabimoreno/presentation/navigation/AppBottomNavigation.kt
+++ gabimoreno/src/main/java/soy/gabimoreno/presentation/navigation/AppBottomNavigation.kt
@@ -1,6 +1,10 @@
 package soy.gabimoreno.presentation.navigation
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -8,7 +12,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppBottomNavigation(
@@ -19,7 +22,8 @@ fun AppBottomNavigation(
         NavItem.entries.forEach { item ->
             val title = stringResource(id = item.titleResId)
             BottomNavigationItem(
-                modifier = Modifier.padding(bottom = 8.dp),
+                modifier = Modifier
+                    .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)),
                 selected = currentRoute.contains(item.navCommand.feature.route),
                 onClick = { onNavItemClick(item) },
                 icon = {
