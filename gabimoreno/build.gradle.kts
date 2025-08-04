f03d7df609b356760106480b6ece7b8553566ba7
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2025-08-04 16:19:01 +0200

    Bump version

--- gabimoreno/build.gradle.kts
+++ gabimoreno/build.gradle.kts
@@ -44,8 +44,8 @@ android {
             libs.versions.sdk.target
                 .get()
                 .toInt()
-        versionCode = 61
-        versionName = "3.4.0"
+        versionCode = 63
+        versionName = "3.6.0"
 
         testInstrumentationRunner = "soy.gabimoreno.di.HiltTestRunner"
         vectorDrawables {
